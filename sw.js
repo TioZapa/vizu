229e798c8c630f4aaf4bcacc962790ac54865037
Matheus Ferreira <69110934+TioZapa@users.noreply.github.com>
2026-05-26 11:06:54 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,17 @@
-const CACHE = 'vizu-v1';
-const ASSETS = [
-  '/vizu/',
-  '/vizu/index.html',
-  '/vizu/manifest.json',
-  '/vizu/clientes.csv',
-  '/vizu/icons/icon-192x192.png',
-  '/vizu/icons/icon-512x512.png'
-];
+const CACHE = 'vizu-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/vizu/index.html')))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
